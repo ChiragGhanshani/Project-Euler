df1b3f6ef6e6684aeb290c799c7d4a92c11e48c1
chirag <chiragghanshani@gmail.com>
2016-03-28 16:56:23 -0500

    Ammended solution to use numbers strictly below 1000

--- src/problemOne.py
+++ src/problemOne.py
@@ -2,6 +2,8 @@ def check(listToCheck):
   return list(filter(lambda x: x % 3 == 0 or x % 5 == 0, listToCheck))
 
 def doSum(listToUse):
-  return reduce(lambda x, y: x + y, check(listToUse))
+  return sum(check(listToUse))
 
-print(doSum(range(1, 1001)))
+print(doSum(range(1, 1000)))
+print(check(range(1, 1000)))
+print(len(check(range(1, 1000))))

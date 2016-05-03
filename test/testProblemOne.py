b57171d605cb5ec9e8b13f56677c0f8e07cc66e4
chirag <chiragghanshani@gmail.com>
2016-05-03 02:26:06 -0500

    modified test

--- test/testProblemOne.py
+++ test/testProblemOne.py
@@ -6,7 +6,7 @@ class EulerTests(unittest.TestCase):
   def test_multiples_of_three_and_five(self):
     passValues = [3, 5, 6, 9, 10]
     test = range(1, 11)
-    assert(set(passValues)== set(check(test)))
+    assert(set(passValues) == set(check(test)))
 
   def test_sum_multiples(self):
     passValue = 33

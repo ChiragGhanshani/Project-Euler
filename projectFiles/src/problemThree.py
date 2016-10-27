787d452c98396e2b4b71bad12c5464cf97c076b5
snosrapkungfu@gmail.com <snosrapkungfu@gmail.com>
2016-10-27 18:43:43 -0500

    refactored for #3

--- projectFiles/src/problemThree.py
+++ projectFiles/src/problemThree.py
@@ -2,11 +2,6 @@ def addNums(x, y):
   return x + y
 
 def findMults(listOfNums):
-  blue1 = []
-  for _i in listOfNums:
-    x = _i
-    if not x % 3 or not x % 5:
-      blue1.extend( [ _i ] )
-  return blue1
+  return [ num for num in listOfNums if num % 3 == 0 or num % 5 == 0 ]
 
 

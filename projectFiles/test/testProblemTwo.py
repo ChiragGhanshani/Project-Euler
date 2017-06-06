438a27b164efd550038f2a8e0aee42a64ae268d1
Chirag Ghanshani <chiragghanshani@gmail.com>
2017-06-06 12:05:47 -0500

    Update testProblemTwo.py
    fixing it again

--- projectFiles/test/testProblemTwo.py
+++ projectFiles/test/testProblemTwo.py
@@ -9,7 +9,7 @@ class EulerTests(unittest.TestCase):
     assert(set(problemTwo.gen_list_under(2)) == set([1]))
 
   def test_gen_list_under_ten(self):
-    assert(set(problemTwo.gen_list_under(10)) == set([1, 3, 5, 8]))
+    assert(set(problemTwo.gen_list_under(10)) == set([1, 2, 3, 5, 8]))
 
   def test_gen_even_fibs_ten(self):
     assert(set(problemTwo.gen_even_fibs_under(10)) == set([2, 8]))
